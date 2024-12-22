f234028ee070539a664611ac619922b3deae572f
kimdev <kim__dev@hufs.ac.kr>
2024-12-22 21:24:14 +0900

    fix(Renderer): 200대 상태의 응답 메시지인 경우에만 statusCode, message 추가

--- community/renderers.py
+++ community/renderers.py
@@ -4,11 +4,11 @@ class CustomRenderer(JSONRenderer):
     
     def render(self, data, accepted_media_type=None, renderer_context=None):
         response_data = renderer_context.get('response')
+        status_code = response_data.status_code
 
-        response = {
-            'statusCode': response_data.status_code,
-            'message': response_data.status_text,
-            'data': data
-        }
+        response = {'data': data}
+        if 200 <= status_code < 300:
+            response['statusCode'] = status_code
+            response['message'] = response_data.status_text
 
         return super(CustomRenderer, self).render(response, accepted_media_type, renderer_context)
